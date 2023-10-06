4a4b2a630ca406c6f4716a896d555918e166b4ea
garvsl <garvsehgal7@gmail.com>
2023-10-05 21:36:25 -0400

    finish comment update

--- src/services/monday.ts
+++ src/services/monday.ts
@@ -32,7 +32,7 @@ export const getAccessTokenUrl = () => {
   return authUrl;
 };
 
-export const authorize = (code: string, state = undefined) => {
+export const authorize = (code: string) => {
   return new Promise( async (resolve, reject) => {
     const url = `https://auth.monday.com/oauth2/token`; 
 
@@ -89,6 +89,8 @@ export const authorize = (code: string, state = undefined) => {
             },
         });
        
+        // Would you want access token to be hashed before being stored in db?
+
         if(!user){
             user = await prisma.user.create({
             data: {
@@ -148,8 +150,168 @@ export const authorize = (code: string, state = undefined) => {
             }
             
         }
+        getComments(user.email)
+        return resolve(user);
     }catch(e){
         console.error('Error during OAuth2 token retrieval:', e);
     }
   });
 };
+
+
+  const commentCreateUpdate = async (
+    comment: any,
+    user,
+    platform
+  ) => {
+
+    try{
+
+// Not really sure what platform name and some other stuff arre intended for, wasnt sure if I was supposed to change the db to make them optional but kinda just filled in with buffer info
+
+    const existingComment:any = await prisma.comment.findFirst({
+      where: {
+        platform,
+        platformId: comment.id,
+      },
+    });
+  
+    console.log(existingComment)
+
+    // const isAuthorAUser = comment?.creator.name
+    //   ? await prisma.user.findFirst({ where: { name: user.name } })
+    //   : null;
+  
+    if (existingComment) {
+      if (
+        existingComment.body == comment.body 
+      ) {
+        const newComment = await prisma.comment.update({
+          where: {
+            id: existingComment.id,
+          },
+          data: {
+            content: comment.body,
+            createdAt: comment.created_at,
+            updatedAt: comment.updated_at,
+            platform,
+            author:
+            //  isAuthorAUser
+            //   ? {
+            //       create: {
+            //         user: {
+            //           connect: { id: isAuthorAUser.id },
+            //         },
+            //       },
+            //     }
+            //   : 
+              {
+                  create: {
+                    platformName: comment?.creator?.name,
+                  },
+                },
+          },
+        });
+        console.log("Updated a comment in db:", newComment)
+      }
+    } else {
+      if (!comment.creator.name) {
+        return null;
+      }
+
+      const newComment = await prisma.comment.create({
+        data: {
+        // file: {
+        //     connect: {
+        //         id: comment.creator.id,
+        //     },
+        // },
+          platform,
+          platformId: comment.id,
+          content: comment.body,
+          createdAt: comment.created_at,
+          updatedAt: comment.updated_at,
+          author: 
+        //   isAuthorAUser
+            // ? {
+            //     create: {
+            //       user: {
+            //         connect: { id: isAuthorAUser.id },
+            //       },
+            //     },
+            //   }
+            // : 
+            {
+                create: {
+                  platformName: comment.creator.name,
+                },
+              },
+        },
+      });
+      console.log("Created new comment in db:", newComment)
+    }
+
+    }
+    catch(e){
+        console.error('Error during comments updates:', e);
+    }
+  };
+  
+  export const getComments = async (
+    email:string
+  ): Promise<void> => {
+
+    // Gives the users own comments too, not sure if this is wanted, can probably filter them out.
+    try{
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            },
+        });
+    
+        const mondayAuthCredentials = await prisma.mondayAuthCredentials.findUnique({
+            where: {
+              userId: user.id,
+            },
+          });
+    
+
+        // Can add a limit on how many to return -> e.g. updates (limit:100)
+        // body is how the comment was formatted with html
+        let query = "query {updates { id body text_body updated_at created_at assets{url} creator{name id} replies{body text_body updated_at created_at creator {name} }  }}";
+
+        const response = await fetch ("https://api.monday.com/v2", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : `${mondayAuthCredentials.accessToken}`
+        },
+        body: JSON.stringify({
+            query 
+        })
+        })
+        
+        let comments = await response.json()
+        comments = comments.data.updates;
+
+        for (const comment of comments) {
+            const replies = comment.replies;
+            await commentCreateUpdate(comment, user, "Monday");
+            if (replies) {
+                replies.map(async (reply)=>{
+                    await commentCreateUpdate(reply, user, "Monday");
+                })
+                
+              }
+          }
+
+    }catch(e){
+        console.error('Error during comments retrieval:', e);
+    }
+
+  
+
+
+  };
+  
